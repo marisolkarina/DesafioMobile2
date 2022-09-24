fa0d190964c65775d14bb78184478f41dc50fa61
marisolkarina <marisol.kari.20@gmail.com>
2022-09-24 03:35:27 -0500

    desafio2

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    primary: '#AA4465',
+    secondary: '#FFA69E',
+    text: '#212121',
+    background: '#ffffff',
+    backgroundLight: '#F5EFED',
+    backgroundDark: '#0F0A0A',
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#9E9E9E',
+}
